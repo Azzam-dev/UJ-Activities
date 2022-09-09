56acb139b573aebafff653e9a4129c3763ad24da
omar0azx <a3mr.azx@gmail.com>
2022-09-10 02:30:08 +0300

    Initial Commit

---
+++ UJ Quizzes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UJ Quizzes
+//
+//  Created by omar alzhrani on 14/02/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
